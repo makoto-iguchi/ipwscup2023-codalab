6440e773c20a2715b2e870d9728cbfd5d8becddf
unknown <makoto.iguchi@kii.com>
2023-06-08 20:18:24 +0900

    Refine code

--- scoring_program_attack/evaluate.py
+++ scoring_program_attack/evaluate.py
@@ -10,7 +10,7 @@ output_dir = sys.argv[2]
 submission_dir = os.path.join(input_dir, 'res')
 orig_dir = os.path.join(input_dir, 'ref')
 
-num_of_team = 10
+num_of_team = 9
 
 with open(os.path.join(submission_dir, "my_team_id.txt")) as my_team:
     my_team_id = int(my_team.read())
@@ -22,28 +22,26 @@ if os.path.isdir(submission_dir) and os.path.isdir(orig_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    score = [1.0] * (num_of_team + 1) # Note: Team ID starts with 1
-    score[0] = 0
-
+    score = [0] * (num_of_team + 1) # Note: Team ID starts with 1
+    
     for i in range(1, num_of_team + 1):
         if i == my_team_id:
-            continue # Preventing suicide attack
+            continue
             
         estimate_file = os.path.join(submission_dir, "estimated_team%02d.csv" % i)
-        answer_file = os.path.join(orig_dir, "correct_team%02d.csv" % i)
+        answer_file = os.path.join(orig_dir, "correct_team%02d_pre.csv" % i)
 
         dfX = pd.read_csv(answer_file, header=None)
         if os.path.exists(estimate_file):
             dfE = pd.read_csv(estimate_file, header=None)
+            score[i] = lmark(dfE, dfX)
         else:
-            continue # No Estimate index submitted. Privacy score will be 1.0
-
-        score[i] = lmark(dfE, dfX)
+            score[i] = 1.0 # No Estimate index submitted. Privacy score will be 1.0
 
     output_filename = os.path.join(output_dir, 'scores.txt')
     with open(output_filename, mode="w") as f:        
-        f.writelines("attack_score:%.4f\n" % (1 - (sum(score) - 1) / (num_of_team - 1))) # Exclude the own data
+        f.writelines("attack_score:%.4f\n" % (1 - (sum(score)) / (num_of_team - 1)))
         for i in range(1, num_of_team + 1):
             if i == my_team_id:
-                continue
+                continue # Preventing suicideal attack
             f.writelines("privacy_score_%d:%.4f\n" % (i, score[i]))
