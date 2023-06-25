11638ee950a6eaec09f5393136c6c228a68f849b
unknown <makoto.iguchi@kii.com>
2023-06-25 22:54:03 +0900

    Support the case of teams not submitting their anonymized data

--- scoring_program_attack/evaluate.py
+++ scoring_program_attack/evaluate.py
@@ -13,9 +13,12 @@ submission_dir = os.path.join(input_dir, 'res')
 orig_dir = os.path.join(input_dir, 'ref')
 
 num_of_team = 10
+skip_team_id = {3} # Team(s) who did not submit their anonymized data
 
+# Read "my team id" and add it to the skip_team_id set
 with open(os.path.join(submission_dir, "my_team_id.txt")) as my_team:
     my_team_id = int(my_team.read())
+    skip_team_id.add(my_team_id)
 
 if not os.path.isdir(submission_dir):
     print("%s doesn't exist" % submission_dir)
@@ -25,33 +28,32 @@ if os.path.isdir(submission_dir) and os.path.isdir(orig_dir):
         os.makedirs(output_dir)
 
     score = [0] * (num_of_team + 1) # Note: Team ID starts with 1
-
+    
     # If there is no estimated_team??.csv file, reject the submission
     if len(glob.glob(os.path.join(submission_dir, "estimated_team??.csv"))) == 0:
         message = "Error: you must submit at least one estimated index file."
         print(message)
         raise Exception(message)
-    
+        
     for i in range(1, num_of_team + 1):
-        if i == my_team_id:
-            continue
+        if i in skip_team_id:
+            continue # Either my team or the team without any submission. Skip
             
         estimate_file = os.path.join(submission_dir, "estimated_team%02d.csv" % i)
         answer_file = os.path.join(orig_dir, "correct_team%02d.csv" % i)
-
         dfX = pd.read_csv(answer_file, header=None)
         if os.path.exists(estimate_file):
             dfE = pd.read_csv(estimate_file, header=None)
             print("estimated_team%d.csv" % i)
-            checkE(dfE) # Format & value check
+            checkE(dfE) # Validate the estimated index format
             score[i] = lmark(dfE, dfX)
         else:
-            score[i] = 1.0 # No Estimate index submitted. Privacy score will be 1.0
+            score[i] = 1.0 # No Estimated index submitted. Set the privacy score to 1.0
 
     output_filename = os.path.join(output_dir, 'scores.txt')
-    with open(output_filename, mode="w") as f:        
-        f.writelines("attack_score:%.4f\n" % (1 - sum(score) / (num_of_team - 1)))
+    with open(output_filename, mode="w") as f:
+        f.writelines("attack_score:%.4f\n" % (1 - sum(score) / (num_of_team - len(skip_team_id))))
         for i in range(1, num_of_team + 1):
-            if i == my_team_id:
-                continue # Preventing suicideal attack
+            if i in skip_team_id:
+                continue
             f.writelines("privacy_score_%d:%.4f\n" % (i, score[i]))
